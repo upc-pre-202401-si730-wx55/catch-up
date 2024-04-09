41e480ae634a203038a72f9e8c449bc5286d1f12
Angel Velasquez <pcisavel@upc.edu.pe>
2024-04-09 14:35:26 -0500

    feat(material-design-ui): added and configured primevue ui library.

--- src/main.js
+++ src/main.js
@@ -2,4 +2,41 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './app.vue'
 
-createApp(App).mount('#app')
+// Add PrimeVue
+import PrimeVue from "primevue/config";
+
+// Add PrimeVue Components
+import Card         from "primevue/card";
+import Button       from "primevue/button";
+import SelectButton from "primevue/selectbutton";
+import Sidebar      from "primevue/sidebar";
+import Avatar       from "primevue/avatar";
+import Menu         from "primevue/menu";
+import Menubar      from "primevue/menubar";
+import Toolbar      from "primevue/toolbar";
+
+// Add PrimeFlex
+import "primeflex/primeflex.css";
+
+// Add PrimeIcons
+import 'primeicons/primeicons.css';
+
+// Add App Theme
+import 'primevue/resources/themes/md-light-indigo/theme.css';
+
+
+const app = createApp(App);
+
+// Add PrimeVue Plugin
+app.use(PrimeVue, { ripple: true})
+    .component('pv-card', Card)
+    .component('pv-button', Button)
+    .component('pv-select-button', SelectButton)
+    .component('pv-sidebar', Sidebar)
+    .component('pv-avatar', Avatar)
+    .component('pv-menu', Menu)
+    .component('pv-menubar', Menubar)
+    .component('pv-toolbar', Toolbar);
+
+// Mount in app div
+app.mount('#app');
