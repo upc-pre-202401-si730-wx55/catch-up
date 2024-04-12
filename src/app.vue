e56ffc7e4e17c1f47325a55ec98977822094a0f6
Angel Velasquez <pcisavel@upc.edu.pe>
2024-04-12 07:58:52 -0500

    feat(main-view): implemented main view component integrating side menu and article list.

---
+++ src/app.vue
@@ -0,0 +1,76 @@
+<script>
+import {NewsApiService} from "./news/services/news-api.service.js";
+import {Article} from "./news/model/article.entity.js";
+import SideMenu from "./news/components/side-menu.component.vue";
+import ArticleList from "./news/components/article-list.component.vue";
+import UnavailableContent from "./news/components/unavailable-content.component.vue";
+
+export default {
+  name: "App",
+  components: {UnavailableContent, ArticleList, SideMenu},
+  data() {
+    return {
+      sidebarVisible: false,
+      articles: [],
+      errors: [],
+      newsApi: new NewsApiService()
+    }
+  },
+  created() {
+    //TODO: Get Articles for default source
+  },
+  methods: {
+    // Build article list from response data
+    buildArticleListFromResponseData(articles) {
+      return articles.map(article => new Article(article.title, article.description, article.url, article.urlToImage, article.source));
+    },
+    // Fetch articles for selected Source
+    getArticlesForSource(sourceId) {
+      this.newsApi.getArticlesForSource(sourceId)
+          .then(response => {
+            let articles = response.data.articles;
+            this.articles = this.buildArticleListFromResponseData(articles);
+            console.log(response.data.articles);
+          }).catch(e => this.errors.push(e));
+    },
+    // Fetch articles for selected Source with Logo URL
+    getArticlesForSourceWithLogo(source) {
+      this.newsApi.getArticlesForSource(source.id)
+          .then(response => {
+            let articles = response.data.articles;
+            this.articles = this.buildArticleListFromResponseData(articles);
+            this.articles.forEach(article => article.source.urlTologo = source.urlToLogo);
+            console.log(response.data.articles);
+          }).catch(e => this.errors.push(e));
+    },
+
+    // Toggle Sidebar
+    toggleSidebar() {
+      this.sidebarVisible = !this.sidebarVisible;
+    },
+
+    // Source selected Event Handler
+    setSource(source) {
+      this.getArticlesForSourceWithLogo(source);
+      this.toggleSidebar();
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="w-full">
+    <div>
+      <pv-menubar class="sticky bg-primary">
+        <template #start>
+          <pv-button icon="pi pi-bars" label="CatchUp" text @click="toggleSidebar"/>
+          <side-menu v-model:visible="sidebarVisible" v-on:source-selected="setSource"/>
+        </template>
+      </pv-menubar>
+    </div>
+  </div>
+  <div>
+    <article-list v-if="errors" :articles="articles"/>
+    <unavailable-content v-else :errors="errors"/>
+  </div>
+</template>
