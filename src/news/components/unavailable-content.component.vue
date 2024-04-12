c12f74068b5fa6ab3da76ce117af5ce1103ece9e
Angel Velasquez <pcisavel@upc.edu.pe>
2024-04-12 07:21:13 -0500

    feat(news): implemented component for unavailable content scenario.

---
+++ src/news/components/unavailable-content.component.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "unavailable-content",
+  props: { errors: null }
+}
+</script>
+
+<template>
+  <div>
+    <div><h4>Unavailable news</h4></div>
+    <div v-for="error in errors">
+      <h6>{{ error }}</h6>
+    </div>
+  </div>
+</template>
