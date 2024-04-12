c70c74436064c2e24342be9ae1c607569220fcd6
Angel Velasquez <pcisavel@upc.edu.pe>
2024-04-12 08:40:33 -0500

    feat(news): added i18n support to read more button.

--- src/news/components/article-card.component.vue
+++ src/news/components/article-card.component.vue
@@ -33,7 +33,7 @@ export default {
       </p>
     </template>
     <template #footer>
-      <a :href="article.url" target="_blank">Read More</a>
+      <a :href="article.url" target="_blank">{{ $t('read-more') }}</a>
     </template>
   </pv-card>
 </template>
