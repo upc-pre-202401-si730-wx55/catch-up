22ad0903f92d8b9a412f6d90383a805f82a4a67d
Angel Velasquez <pcisavel@upc.edu.pe>
2024-04-12 08:29:10 -0500

    feat(footer): added footer section with i18n support.

---
+++ src/public/components/footer-content.component.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "footer-content"
+}
+</script>
+
+<template>
+  <div class="grid bg-primary mt-4 p-4 align-content-start">
+    <div class="col-12 ml-3 align-items-center justify-content-center">
+      <p>Copyright &copy; 2024. ACME Studio</p>
+    </div>
+    <div class="col-12 ml-3 align-items-center justify-content-center">
+      <p>
+        {{ $t('authoring-phrase.intro')}} <i class="pi pi-heart text-red-50"/>
+        {{ $t('authoring-phrase.use')}} <a class="text-white" href="https://primevue.org" target="_blank">PrimeVue</a>
+        {{ $t('authoring-phrase.author', { brand: 'ACME'})}}
+      </p>
+    </div>
+  </div>
+</template>
